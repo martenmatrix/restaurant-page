0e62205889f71b9bac1ffc41fc4cd30ecc921778
Marten <derapfelesser@aol.com>
2021-08-24 23:27:29 +0200

    Add CSS asset settings to webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './style.css';
 import createHomepage from "./createPage";
 
 createHomepage.homepage();
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,13 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  
+
+  modules: {
+      rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+      ],
+  },
 };
