f1a26716d97937aae2420158066e7277c2bd4c5c
Marten <derapfelesser@aol.com>
2021-08-26 00:14:01 +0200

    Add products and images to product object

--- src/products.js
+++ src/products.js
@@ -1,7 +1,39 @@
+import bologneseImage from './img/products/bolognese.png';
+import bisteccaImage from './img/products/bistecca.png';
+import centralImage from './img/products/central.png';
+import napoliImage from './img/products/napoli.png';
+import orlandoImage from './img/products/orlando.png';
+import pasadenaImage from './img/products/pasadena.png';
+
 const products = [
     {
-        title: 'Pizza',
-        image: '',
+        title: 'Bolognese',
+        image: bologneseImage,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptate.',
+    },
+    {
+        title: 'Bistecca',
+        image: bisteccaImage,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptate.',
+    },
+    {
+        title: 'Central',
+        image: centralImage,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptate.',
+    },
+    {
+        title: 'Napoli',
+        image: napoliImage,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptate.',
+    },
+    {
+        title: 'Orlando',
+        image: orlandoImage,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptate.',
+    },
+    {
+        title: 'Pasadena',
+        image: pasadenaImage,
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, voluptate.',
     },
 ];
